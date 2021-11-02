96a9b7fb048d1a69ea81e3e380d81fa49bdb8c69
Hassan Habib <hassanhabib@live.com>
2021-11-01 20:28:33 -0700

    INFRA: Initialize Unit Tests Project

---
+++ Sofee.Core/Sofee.Core.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Xunit;
+
+namespace Sofee.Core.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
