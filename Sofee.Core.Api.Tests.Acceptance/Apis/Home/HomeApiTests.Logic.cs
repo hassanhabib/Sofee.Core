aa3c9d2050e4b7413121b4b5824f3036875ea5d9
Hassan Habib <hassanhabib@live.com>
2021-11-02 20:52:47 -0700

    INFRA: Initialize Acceptance Tests

---
+++ Sofee.Core.Api.Tests.Acceptance/Apis/Home/HomeApiTests.Logic.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Sofee.Core.Api.Tests.Acceptance.Apis.Home
+{
+    public partial class HomeApiTests
+    {
+        [Fact]
+        public async Task ShouldReturnHomeMessageAsync()
+        {
+            // given
+            string expectedHomeMessage =
+                "Hello Mario, the princess is in another castle.";
+
+            // when
+            string actualHomeMessage =
+                await this.sofeeApiBroker.GetHomeMessageAsync();
+
+            // then
+            actualHomeMessage.Should().BeEquivalentTo(expectedHomeMessage);
+        }
+    }
+}
---
+++ Sofee.Core.Api.Tests.Acceptance/Brokers/SofeeApiBroker.Home.cs
@@ -0,0 +1,17 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace Sofee.Core.Api.Tests.Acceptance.Brokers
+{
+    public partial class SofeeApiBroker
+    {
+        private const string HomeRelativeUrl = "api/home";
+
+        public async ValueTask<string> GetHomeMessageAsync() =>
+            await this.apiFactoryClient.GetContentStringAsync(HomeRelativeUrl);
+    }
+}
