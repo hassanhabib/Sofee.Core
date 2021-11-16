0ee1a955f72e2353ab41a2b8dba4f73cb3b44723
Hassan Habib <hassanhabib@live.com>
2021-11-15 20:48:12 -0800

    PROVISION: Initialize Cloud Broker

---
+++ Sofee.Core.Api.Infrastructure.Provision/Program.cs
@@ -0,0 +1,17 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+
+namespace Sofee.Core.Api.Infrastructure.Provision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
