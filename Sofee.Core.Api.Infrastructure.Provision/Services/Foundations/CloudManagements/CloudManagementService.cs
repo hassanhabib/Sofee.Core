b904434712f358911d5da17aa776dcabf24cee12
Hassan Habib <hassanhabib@live.com>
2021-11-25 20:20:07 -0800

    FOUNDATIONS: Implement Provisioning Resource Group

---
+++ Sofee.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
@@ -0,0 +1,46 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Sofee.Core.Api.Infrastructure.Provision.Brokers.Clouds;
+using Sofee.Core.Api.Infrastructure.Provision.Brokers.Loggings;
+
+namespace Sofee.Core.Api.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public class CloudManagementService : ICloudManagementService
+    {
+        private readonly ICloudBroker cloudBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CloudManagementService(
+            ICloudBroker cloudBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.cloudBroker = new CloudBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        public async ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment)
+        {
+            string resourceGroupName = $"{projectName}-RESOURCES-{environment}".ToUpper();
+            this.loggingBroker.LogActivity(message: $"Provisioning {resourceGroupName}...");
+
+            IResourceGroup resourceGroup =
+                await this.cloudBroker.CreateResourceGroupAsync(
+                    resourceGroupName);
+
+            this.loggingBroker.LogActivity(message: $"{resourceGroupName} Provisioned.");
+
+            return resourceGroup;
+        }
+    }
+}
---
+++ Sofee.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/ICloudManagementService.cs
@@ -0,0 +1,17 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace Sofee.Core.Api.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public interface ICloudManagementService
+    {
+        ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment);
+    }
+}
