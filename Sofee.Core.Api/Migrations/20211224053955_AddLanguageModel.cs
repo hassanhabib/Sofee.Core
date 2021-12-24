19e36060fd07e9e6af7d0af962cfd65461a5f8b2
Yeasin Arafat <yeasin@s3innovate.com>
2021-12-24 11:43:46 +0600

    DATA: Language Models & Migrations

---
+++ Sofee.Core.Api/Migrations/20211224053955_AddLanguageModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sofee.Core.Api.Migrations
+{
+    public partial class AddLanguageModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ISO = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Languages");
+        }
+    }
+}
---
+++ Sofee.Core.Api/Models/Languages/Language.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sofee.Core.Api.Models.Languages
+{
+    public class Language
+    {
+        public Guid Id { get; set; }
+        public string ISO { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
