1f158e307e781381cc6e4e78e81512a86e258cae
Hassan Habib <hassanhabib@live.com>
2021-12-28 17:58:57 -0800

    BROKERS: Insert Language

--- Sofee.Core.Api/Brokers/Storages/StorageBroker.Language.cs
+++ Sofee.Core.Api/Brokers/Storages/StorageBroker.Language.cs
@@ -3,7 +3,9 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Sofee.Core.Api.Models.Languages;
 
 namespace Sofee.Core.Api.Brokers.Storages
@@ -11,5 +13,17 @@ namespace Sofee.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Language> Languages { get; set; }
+
+        public async ValueTask<Language> InsertLanguageAsync(Language language)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Language> languageEntityEntry =
+                await broker.Languages.AddAsync(language);
+
+            await broker.SaveChangesAsync();
+
+            return languageEntityEntry.Entity; 
+        }
     }
 }
