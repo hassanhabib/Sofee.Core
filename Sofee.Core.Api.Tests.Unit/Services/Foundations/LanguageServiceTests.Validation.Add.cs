b061b09fbb9948ae0711154e2ce93568864a87d2
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2022-01-03 02:57:20 -0800

    ShouldThrowValidationExceptionOnAddIfLanguageIsNull() -> FAIL

---
+++ Sofee.Core.Api.Tests.Unit/Services/Foundations/LanguageServiceTests.Validation.Add.cs
@@ -0,0 +1,46 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sofee.Core.Api.Models.Languages;
+using Sofee.Core.Api.Models.Languages.Exceptions;
+using Xunit;
+
+namespace Sofee.Core.Api.Tests.Unit.Services.Foundations
+{
+    public partial class LanguageServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfLanguageIsNull()
+        {
+            //given 
+            Language nullLanguage = null;
+
+            var nullLanguageException =
+                    new NullLanguageException();
+
+            var expectedLanguageValidationException =
+                    new LanguageValidationException(nullLanguageException);
+
+            //when
+            ValueTask<Language> addLanguageTask =
+                 this.languageService.AddLanguageAsync(nullLanguage);
+
+            //then
+            await Assert.ThrowsAsync<LanguageValidationException>(() =>
+                addLanguageTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                   broker.InsertLanguageAsync(nullLanguage),
+                       Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
