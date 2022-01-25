58bcf5073212ba7b4b9e753c83c4d19e6dd055b3
Nesad Kadric <31567213+nesadkadric@users.noreply.github.com>
2022-01-25 08:40:10 +0100

    SERVICE: Language

---
+++ Sofee.Core.Api/Services/Foundations/Languages/ILanguageService.cs
@@ -0,0 +1,16 @@
+﻿using Sofee.Core.Api.Models.Languages;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sofee.Core.Api.Services.Foundations.Languages
+{
+    public interface ILanguageService
+    {
+        ValueTask<Language> AddLanguageAsync(Language language);
+        ValueTask<Language> RetrieveLanguageByIdAsync(Guid languageId);
+        IQueryable<Language> RetrieveAllLanguages();
+        ValueTask<Language> ModifyLanguageAsync(Language language);
+        ValueTask<Language> RemoveLanguageByIdAsync(Guid languageId);
+    }
+}
---
+++ Sofee.Core.Api/Services/Foundations/Languages/LanguageService.cs
@@ -0,0 +1,43 @@
+﻿using Sofee.Core.Api.Brokers.Storages;
+using Sofee.Core.Api.Models.Languages;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sofee.Core.Api.Services.Foundations.Languages
+{
+    public class LanguageService : ILanguageService
+    {
+        private readonly IStorageBroker storage;
+
+        public LanguageService(IStorageBroker storage)
+        {
+            this.storage = storage;
+        }
+
+        public ValueTask<Language> AddLanguageAsync(Language language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Language> ModifyLanguageAsync(Language language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Language> RemoveLanguageByIdAsync(Guid languageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Language> RetrieveAllLanguages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Language> RetrieveLanguageByIdAsync(Guid languageId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
