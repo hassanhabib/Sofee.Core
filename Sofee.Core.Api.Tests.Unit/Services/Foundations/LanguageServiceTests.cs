85255f1a298e6a1f704dd454d7f6c6c832e99d15
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2022-01-31 19:33:26 -0800

    ShouldAddLanguageAsync -> FAIL

---
+++ Sofee.Core.Api.Tests.Unit/Services/Foundations/LanguageServiceTests.cs
@@ -0,0 +1,52 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using Moq;
+using Sofee.Core.Api.Brokers.Loggings;
+using Sofee.Core.Api.Brokers.Storages;
+using Sofee.Core.Api.Models.Languages;
+using Sofee.Core.Api.Services.Foundations.Languages;
+using Tynamix.ObjectFiller;
+
+namespace Sofee.Core.Api.Tests.Unit.Services.Foundations
+{
+    public partial class LanguageServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ILanguageService languageService;
+
+        public LanguageServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.languageService = new LanguageService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Language CreateRandomLanguage() =>
+                CreateLanguageFiller().Create();
+
+        private static Filler<Language> CreateLanguageFiller()
+        {
+            var filler = new Filler<Language>();
+            Guid createdById = Guid.NewGuid();
+
+            filler.Setup()
+                .OnProperty(language => language.CreatedDate).Use(GetRandomDateTime())
+                .OnProperty(language => language.UpdatedDate).IgnoreIt()
+                .OnProperty(language => language.CreatedBy).Use(createdById)
+                .OnProperty(language => language.UpdatedBy).IgnoreIt();
+
+            return filler;
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+    }
+}
---
+++ Sofee.Core.Api/Services/Foundations/Languages/LanguageService.cs
@@ -0,0 +1,32 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Sofee.Core.Api.Brokers.Loggings;
+using Sofee.Core.Api.Brokers.Storages;
+using Sofee.Core.Api.Models.Languages;
+
+namespace Sofee.Core.Api.Services.Foundations.Languages
+{
+    public partial class LanguageService : ILanguageService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public LanguageService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<Language> AddLanguageAsync(Language language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
