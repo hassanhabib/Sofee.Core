9022780cc02a237f8ff7520377369dd3205caf37
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2022-02-06 23:05:59 -0800

    ShouldThrowValidationExceptionOnAddIfLanguageIsInvalidAndLogItAsync -> FAIL

--- Sofee.Core.Api.Tests.Unit/Services/Foundations/LanguageServiceTests.Validation.Add.cs
+++ Sofee.Core.Api.Tests.Unit/Services/Foundations/LanguageServiceTests.Validation.Add.cs
@@ -45,5 +45,79 @@ namespace Sofee.Core.Api.Tests.Unit.Services.Foundations
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfLanguageIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given 
+            var invalidLanguage = new Language
+            {
+                ISO = invalidText,
+                Text = invalidText
+            };
+
+            var invalidLanguageException =
+                new InvalidLanguageException();
+
+            invalidLanguageException.AddData(
+                key: nameof(Language.Id),
+                values: "Id is required");
+
+            invalidLanguageException.AddData(
+                key: nameof(Language.ISO),
+                values: "Text is required");
+
+            invalidLanguageException.AddData(
+                key: nameof(Language.Text),
+                values: "Text is required");
+
+            invalidLanguageException.AddData(
+                key: nameof(Language.CreatedDate),
+                values: "Date is required");
+
+            invalidLanguageException.AddData(
+                key: nameof(Language.CreatedBy),
+                values: "Id is required");
+
+            invalidLanguageException.AddData(
+                key: nameof(Language.UpdatedDate),
+                values: "Date is required");
+
+            invalidLanguageException.AddData(
+                key: nameof(Language.UpdatedBy),
+                values: "Id is required");
+
+            var expectedLanguageValidationException =
+                new LanguageValidationException(invalidLanguageException);
+
+            // when 
+            ValueTask<Language> addLanguageTask =
+                this.languageService.AddLanguageAsync(invalidLanguage);
+
+            // then
+            await Assert.ThrowsAsync<LanguageValidationException>(() =>
+                addLanguageTask.AsTask());
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedLanguageValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertLanguageAsync(It.IsAny<Language>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
