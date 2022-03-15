959dd072e07b69c7cfc1102782b4ca825d214ba8
Khondamir Abdukhoshimov <iCloudBMX0525@gmail.com>
2022-03-15 12:53:24 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS ShouldThrowServiceExceptionOnRetrieveByIdIfDatabaseUpdateErrorOccursAndLogItAsync -> PASS

---
+++ Sofee.Core.Api.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,98 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Sofee.Core.Api.Models.Languages;
+using Sofee.Core.Api.Models.Languages.Exceptions;
+using Xunit;
+
+namespace Sofee.Core.Api.Tests.Unit.Services.Foundations.Languages
+{
+    public partial class LanguageServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedLanguageStorageException =
+                new FailedLanguageStorageException(sqlException);
+
+            var expectedLanguageDependencyException =
+                new LanguageDependencyException(failedLanguageStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectLanguageByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Language> retrieveLanguageByIdTask =
+                this.languageService.RetrieveLanguageByIdAsync(someId);
+
+            // then
+            await Assert.ThrowsAsync<LanguageDependencyException>(() => 
+                retrieveLanguageByIdTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectLanguageByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedLanguageDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdIfDatabaseUpdateErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedLanguageServiceException =
+                new FailedLanguageServiceException(serviceException);
+
+            var expectedLanguageServiceException =
+                new LanguageServiceException(failedLanguageServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectLanguageByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Language> retrieveLanguageByIdTask =
+                this.languageService.RetrieveLanguageByIdAsync(someId);
+
+            // then
+            await Assert.ThrowsAsync<LanguageServiceException>(() =>
+                retrieveLanguageByIdTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectLanguageByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedLanguageServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
