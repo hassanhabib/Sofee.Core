f81a9ca4539a2ad7f277b42e5d2c25b9fe3b3fa7
Khondamir Abdukhoshimov <iCloudBMX0525@gmail.com>
2022-03-15 10:54:50 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ Sofee.Core.Api.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.Validation.RetrieveById.cs
@@ -0,0 +1,55 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using Sofee.Core.Api.Models.Languages;
+using Sofee.Core.Api.Models.Languages.Exceptions;
+using Xunit;
+
+namespace Sofee.Core.Api.Tests.Unit.Services.Foundations.Languages
+{
+    public partial class LanguageServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidLanguageId = Guid.Empty;
+
+            var invalidLanguageException =
+                new InvalidLanguageException();
+
+            invalidLanguageException.AddData(
+                key: nameof(Language.Id),
+                values: "Id is required");
+
+            var expectedLanguageValidationException = 
+                new LanguageValidationException(invalidLanguageException);
+
+            // when
+            ValueTask<Language> retrieveLanguageByIdTask = 
+                this.languageService.RetrieveLanguageByIdAsync(invalidLanguageId);
+
+            // then
+            await Assert.ThrowsAsync<LanguageValidationException>(() =>
+                retrieveLanguageByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedLanguageValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectLanguageByIdAsync(It.IsAny<Guid>()),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls(); 
+        }
+    }
+}
