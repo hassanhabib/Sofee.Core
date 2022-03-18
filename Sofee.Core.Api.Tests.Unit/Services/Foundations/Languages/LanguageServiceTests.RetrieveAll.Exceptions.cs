c75b5618134b4c24249cd291329d1f07dca9bf63
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-03-18 19:35:00 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccurredAndLogIt -> FAIL

---
+++ Sofee.Core.Api.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.RetrieveAll.Exceptions.cs
@@ -0,0 +1,42 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using Sofee.Core.Api.Models.Languages;
+using Xunit;
+
+namespace Sofee.Core.Api.Tests.Unit.Services.Foundations.Languages
+{
+    public partial class LanguageServiceTests
+    {
+        [Fact]
+        public void ShouldReturnLanguages()
+        {
+            // given
+            IQueryable<Language> randomLanguages = CreateRandomLanguages();
+            IQueryable<Language> storageLanguages = randomLanguages;
+            IQueryable<Language> expectedLanguages = randomLanguages;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllLanguages())
+                    .Returns(storageLanguages);
+
+            // when
+            IQueryable<Language> actualLanguages =
+                this.languageService.RetrieveAllLanguages();
+
+            // then
+            actualLanguages.Should().BeEquivalentTo(expectedLanguages);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllLanguages(), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Sofee.Core.Api.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.RetrieveAll.Logic.cs
+++ Sofee.Core.Api.Tests.Unit/Services/Foundations/Languages/LanguageServiceTests.RetrieveAll.Logic.cs
@@ -3,10 +3,13 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
+using System;
 using System.Linq;
 using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using Moq;
 using Sofee.Core.Api.Models.Languages;
+using Sofee.Core.Api.Models.Languages.Exceptions;
 using Xunit;
 
 namespace Sofee.Core.Api.Tests.Unit.Services.Foundations.Languages
@@ -14,29 +17,42 @@ namespace Sofee.Core.Api.Tests.Unit.Services.Foundations.Languages
     public partial class LanguageServiceTests
     {
         [Fact]
-        public void ShouldReturnLanguages()
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccurredAndLogIt()
         {
             // given
-            IQueryable<Language> randomLanguages = CreateRandomLanguages();
-            IQueryable<Language> storageLanguages = randomLanguages;
-            IQueryable<Language> expectedLanguages = randomLanguages;
+            IQueryable<Language> someLangues = CreateRandomLanguages();
+            SqlException sqlException = GetSqlException();
+
+            var failedLanguageStoragexception =
+                new FailedLanguageStorageException(sqlException);
+
+            var expectedLanguageDependencyException =
+                new LanguageDependencyException(failedLanguageStoragexception);
 
             this.storageBrokerMock.Setup(broker =>
                 broker.SelectAllLanguages())
-                    .Returns(storageLanguages);
+                    .Throws(sqlException);
 
-            // when
-            IQueryable<Language> actualLanguages =
+            // when 
+            Action retrieveAllLanguagesAction = () =>
                 this.languageService.RetrieveAllLanguages();
 
             // then
-            actualLanguages.Should().BeEquivalentTo(expectedLanguages);
+            Assert.Throws<LanguageDependencyException>(retrieveAllLanguagesAction);
 
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectAllLanguages(), Times.Once);
 
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(
+                    SameExceptionAs(
+                        expectedLanguageDependencyException))),
+                            Times.Once);
+
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
+
 }
