985008e6aba1960532135bb65566000810bb2110
hoxhaleo319 <burim@piorsoft.com>
2022-04-06 21:39:47 -0700

    BROKERS: Insert Country

---
+++ Sofee.Core.Api/Brokers/Storages/IStorageBroker.Countries.cs
@@ -0,0 +1,15 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Sofee.Core.Api.Models.Countries;
+using System.Threading.Tasks;
+
+namespace Sofee.Core.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Country> InsertCounrtyAsync(Country country);
+    }
+}
