a44a5809d934c98a25794919c83b0282ec183130
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-11-27 10:34:26 +0300

    ShouldAddCountryAsync ->FAIL

---
+++ Sofee.Core.Api.Tests.Unit/Services/Foundations/Countries/CountryServiceTests.cs
@@ -0,0 +1,49 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Moq;
+using Sofee.Core.Api.Brokers.Loggings;
+using Sofee.Core.Api.Brokers.Storages;
+using Sofee.Core.Api.Models.Countries;
+using Sofee.Core.Api.Services.Foundations.Countries;
+using System;
+using Tynamix.ObjectFiller;
+
+namespace Sofee.Core.Api.Tests.Unit.Services.Foundations.Countries
+{
+    public partial class CountryServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ICountryService countryService;
+
+        public CountryServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.countryService = new CountryService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+           new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Country CreateRandomCountry() =>
+           CreateCountryFiller().Create();
+
+        private static Filler<Country> CreateCountryFiller()
+        {
+            var filler = new Filler<Country>();
+            DateTimeOffset dates = GetRandomDateTime();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
---
+++ Sofee.Core.Api/Services/Foundations/Countries/ICountryService.cs
@@ -0,0 +1,16 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Sofee.Core.Api.Brokers.Storages;
+using Sofee.Core.Api.Models.Countries;
+using System.Threading.Tasks;
+
+namespace Sofee.Core.Api.Services.Foundations.Countries
+{
+    public interface ICountryService
+    {
+        ValueTask<Country> AddCountryAsync(Country country);
+    }
+}
