62a3590cc42792d0d56f672d8b83d1c4747c6daa
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-12-01 08:12:09 +0300

    CONFIG: Upgrade to.NET 7.0.0

--- Sofee.Core.Api.Infrastructure.Build/Program.cs
+++ Sofee.Core.Api.Infrastructure.Build/Program.cs
@@ -44,7 +44,7 @@ var githubPipeline = new GithubPipeline
 
                     TargetDotNetVersion = new TargetDotNetVersion
                     {
-                        DotNetVersion = "6.0.101",
+                        DotNetVersion = "7.0.100",
                         IncludePrerelease = true
                     }
                 },
